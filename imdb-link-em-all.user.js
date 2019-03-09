3699ee37ff09a3df381eebf3ef732f060942d355
buzz <buzz@users.noreply.github.com>
2019-03-09 14:18:21 +0100

    fix filelist

--- imdb-link-em-all.user.js
+++ imdb-link-em-all.user.js
@@ -729,7 +729,7 @@ const sites = [
       'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQBAMAAADt3eJSAAAAElBMVEUsLS8pTFsmZHwje54shKUfotVCbnToAAAANElEQVQI12NwgQIGBwYwYMHLYAwNFYUwAhjgDFUwgyk0EMJgDmAwhegKYGANDWAhymSYMwBdTQvkUareoQAAAABJRU5ErkJggg==',
       'http://filelist.ro/browse.php?search=tt{{IMDB_ID}}&cat=0&searchin=0&sort=0',
       'Try again with a different search string',
-      'Not logged in!'
+      'FileList :: Login'
     ]
   },
 
