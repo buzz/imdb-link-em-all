7bfcb766023e3d807e287c2188cd7f892f750212
buzz <buzz@users.noreply.github.com>
2020-08-28 13:44:37 +0200

    Fill about tab

--- src/components/Config/About.jsx
+++ src/components/Config/About.jsx
@@ -1,29 +1,69 @@
-import { h, Fragment } from 'preact'
+import { h } from 'preact'
 
 import { DESCRIPTION, HOMEPAGE, GREASYFORK_URL, NAME_VERSION } from 'imdb-link-em-all/constants'
+import css from 'imdb-link-em-all/components/Config/About.sss'
 
 const About = () => (
-  <>
-    <h3>{NAME_VERSION}</h3>
-    <p>{DESCRIPTION}</p>
-    <ul>
-      <li>
-        <a target="_blank" rel="noreferrer" href={HOMEPAGE}>
-          GitHub
+  <div className={css.about}>
+    <div className={css.top}>
+      <h3>🎥 {NAME_VERSION}</h3>
+      <p>{DESCRIPTION}</p>
+    </div>
+    <div className={css.content}>
+      <h2>🔗 Links</h2>
+      <ul>
+        <li>
+          <a target="_blank" rel="noreferrer" href={HOMEPAGE}>
+            GitHub
+          </a>
+        </li>
+        <li>
+          <a target="_blank" rel="noreferrer" href={GREASYFORK_URL}>
+            Greasy Fork
+          </a>
+        </li>
+      </ul>
+      <h2>✨ Contributions</h2>
+      <p>Add new sites or update existing entries.</p>
+      <ul>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://github.com/buzz/imdb-link-em-all/issues/new"
+          >
+            Open a GitHub issue
+          </a>{' '}
+          or
+        </li>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://greasyfork.org/en/scripts/17154-imdb-link-em-all/feedback"
+          >
+            Give feedback
+          </a>{' '}
+          on Greasy Fork.
+        </li>
+      </ul>
+      <p>
+        <em>Thanks to all the contributors!</em> 👍
+      </p>
+      <h2>⚖ License</h2>
+      <p>
+        This script is licensed under the terms of the{' '}
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/buzz/imdb-link-em-all/blob/master/LICENSE"
+        >
+          GPL-2.0 License
         </a>
-      </li>
-      <li>
-        <a target="_blank" rel="noreferrer" href={GREASYFORK_URL}>
-          Greasy Fork
-        </a>
-      </li>
-      <li>
-        <a href="https://github.com/buzz/imdb-link-em-all/blob/master/ADDING-SITES.md">
-          Add new sites
-        </a>
-      </li>
-    </ul>
-  </>
+        .
+      </p>
+    </div>
+  </div>
 )
 
 export default About
