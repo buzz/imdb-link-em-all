87f43fc21322147b679e5ca230e929f738b61ec2
buzz <buzz@users.noreply.github.com>
2021-07-12 21:34:53 +0200

    v2.0.9

--- imdb-link-em-all.user.js
+++ imdb-link-em-all.user.js
@@ -10,7 +10,7 @@
 // @license     GPLv2
 // @noframes
 // @author      buzz
-// @version     2.0.8
+// @version     2.0.9
 // @grant       GM_getValue
 // @grant       GM_setValue
 // @grant       GM_xmlhttpRequest
@@ -21,7 +21,7 @@
 (function (preact, hooks) {
   'use strict';
 
-  var version = "2.0.8";
+  var version = "2.0.9";
   var description = "Adds all kinds of links to IMDb, customizable!";
   var homepage = "https://github.com/buzz/imdb-link-em-all#readme";
 
