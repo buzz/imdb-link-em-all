9139aba9427941df927c2f36a2f06961ce95e8b0
buzz <buzz@users.noreply.github.com>
2026-01-17 12:35:04 +0100

    Handle errors inside injectAndStart

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,28 @@ import { CONTAINER_ID } from 'imdb-link-em-all/constants'
 import App from 'imdb-link-em-all/components/App'
 import { detectLayout, failureLog, parseImdbInfo, successLog } from 'imdb-link-em-all/util'
 
+function injectAndStart(imdbInfo, containerSelector) {
+  try {
+    let injectionEl = document.querySelector(containerSelector)
+    if (!injectionEl) {
+      throw new Error('Could not find target container')
+    }
+
+    const container = document.createElement('div')
+    container.id = CONTAINER_ID
+    container.className = 'ipc-page-content-container ipc-page-content-container--center'
+    container.style.position = 'relative'
+    container.style.padding = '0 var(--ipt-pageMargin)'
+    container.style.minHeight = '50px'
+
+    injectionEl.prepend(container)
+    render(<App imdbInfo={imdbInfo} />, container)
+    successLog(imdbInfo.layout)
+  } catch (error) {
+    failureLog(error)
+  }
+}
+
 try {
   // Parse IMDb number and layout
   const mUrl = /^\/(?:[a-z]{2}\/)?title\/tt([0-9]{7,8})(?:\/([a-z]*))?/.exec(
@@ -11,7 +33,7 @@ try {
   )
 
   if (!mUrl) {
-    throw new Error('Could not parse IMDb URL!')
+    throw new Error('Could not parse IMDb URL')
   }
 
   // Only enable on title page and reference layout
@@ -22,33 +44,15 @@ try {
     const layoutInfo = detectLayout(mUrl)
     const [imdbInfo, containerSelector] = parseImdbInfo(imdbId, layoutInfo)
 
-    function injectAndStart() {
-      let injectionEl = document.querySelector(containerSelector)
-      if (!injectionEl) {
-        throw new Error('Could not find target container!')
-      }
-
-      const container = document.createElement('div')
-      container.id = CONTAINER_ID
-      container.className = 'ipc-page-content-container ipc-page-content-container--center'
-      container.style.position = 'relative'
-        container.style.padding = '0 var(--ipt-pageMargin)'
-        container.style.minHeight = '50px'
-
-      injectionEl.prepend(container)
-      render(<App imdbInfo={imdbInfo} />, container)
-      successLog(imdbInfo.layout)
-    }
-
-    function containerWatchdog() {
+    function presenceInjector() {
       const container = document.querySelector(`#${CONTAINER_ID}`)
       if (container === null) {
-        injectAndStart()
+        injectAndStart(imdbInfo, containerSelector)
       }
-      window.setTimeout(containerWatchdog, 1000)
+      window.setTimeout(presenceInjector, 1000)
     }
 
-    window.setTimeout(containerWatchdog, 500)
+    window.setTimeout(presenceInjector, 500)
   }
 } catch (error) {
   failureLog(error)
